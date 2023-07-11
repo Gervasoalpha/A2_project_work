dc6dab0eb74b7c368de9515bf814cf24e97f4cad
JACKPOT <JACKPOT@JACOPO-PC>
2023-07-11 22:42:08 +0200

    made join for logs

--- Backend/Api/A2-project-work.ApplicationCore/Interfaces/Repositories/ILogRepository.cs
+++ Backend/Api/A2-project-work.ApplicationCore/Interfaces/Repositories/ILogRepository.cs
@@ -12,5 +12,9 @@ namespace A2_project_work.ApplicationCore.Interfaces.Repositories
         Task<long> GetLastPicLogId(Guid pic_id);
 
         Task<string> GetUnlockCode(string auhtcode);
+
+        Task UpdateUser(UsernameUserId us);
+
+        Task<IEnumerable<GetLog>> PrettyGet();
     }
 }
